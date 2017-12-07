56fc66af8975ff1edb3eaf92283cd1ca6f5511d4
Redpike <rsokulski@op.pl>
2017-12-07 21:17:23 +0100

    Advent of Code 2017 - Day 7 Part 2 done

--- 2017/d07/d07.py
+++ 2017/d07/d07.py
@@ -1,7 +1,6 @@
 import re
 from collections import defaultdict
 
-
 test_input = [
     'pbga (66)',
     'xhth (57)',
@@ -21,6 +20,7 @@ test_input = [
 check_children_pattern = '.*->.*'
 node_with_children_pattern = '^(\w+) \((\d+)\)(?: -> )?(.*)$'
 node_without_children_pattern = '^(\w+) \((\d+)\)$'
+new_balanced_weight = 0
 
 
 class Node:
@@ -37,15 +37,7 @@ class Node:
         self.node_children_weight = node_children_weight
 
     def __str__(self):
-        return self.name + ': nw: ' + str(self.node_weight) + ' ndw: ' + str(self.node_children_weight)
-
-
-    def getParent(self):
-        return self.parent
-
-
-    def getChildren(self):
-        return self.children
+        return self.name
 
 
 def readInputFile():
@@ -70,7 +62,6 @@ def createNode(regex_node_line, with_children):
     return node
 
 
-
 def createProgramTree(input_data):
     program_tree = defaultdict()
     for node_line in input_data:
@@ -93,15 +84,33 @@ def putParentIntoNodes(program_tree):
             child_in_program_tree.parent = tree_element
 
 
-def putWeightsOfChildrenToParent(program_tree):
-    for tree_element in program_tree.keys():
-        children_in_node = program_tree.get(tree_element).children
-        sum_of_weights = program_tree.get(tree_element).node_children_weight
-        for child_name in children_in_node:
-            children_node_weight = program_tree.get(child_name).node_weight
-            sum_of_weights += int(children_node_weight)
-        program_tree.get(tree_element).node_children_weight = sum_of_weights
-        print(program_tree.get(tree_element))
+#TODO Need refactor!!!!!!!
+def putWeightsOfChildrenToParent(program_tree, current_node):
+    good_weight = 0
+    bad_weight = 0
+    children_weights = []
+    children = program_tree.get(current_node).children
+    for child_name in children:
+        if len(children) > 0:
+            child_support = putWeightsOfChildrenToParent(program_tree, child_name)
+            weight = sum(child_support) + int(program_tree.get(child_name).node_weight)
+        else:
+            weight = program_tree.get(child_name).node_weight
+        program_tree.get(child_name).node_children_weight = weight
+        if program_tree.get(current_node).parent is not None:
+            children_weights.append(weight)
+    if len(set(children_weights)) > 1:
+        for weight in children_weights:
+            if children_weights.count(weight) == 1:
+                bad_weight = weight
+            else:
+                good_weight = weight
+        substract = abs(good_weight - bad_weight)
+        for child_name in program_tree.get(current_node).children:
+            if program_tree.get(child_name).node_children_weight == bad_weight:
+                global new_balanced_weight
+                new_balanced_weight = program_tree.get(child_name).node_weight - substract
+    return children_weights
 
 
 def giveMeThisShit(program_tree):
@@ -110,16 +119,21 @@ def giveMeThisShit(program_tree):
             return tree_element
 
 
-def getBottomProgram(input_data):
+def getBottomProgram(input_data, part):
     program_tree = createProgramTree(input_data)
     putParentIntoNodes(program_tree)
-    putWeightsOfChildrenToParent(program_tree)
     name_of_bottom_program_node = giveMeThisShit(program_tree)
-    return name_of_bottom_program_node
+    if part == 1:
+        return name_of_bottom_program_node
+    else:
+        #TODO Move to another method
+        good_weight = putWeightsOfChildrenToParent(program_tree, name_of_bottom_program_node)
+        return good_weight
 
 
 def test():
-    assert getBottomProgram(test_input) == 'tknk'
+    assert getBottomProgram(test_input, 1) == 'tknk'
+    # assert getBottomProgram(test_input, 2) == 60
 
 
 def selectInput(is_production):
@@ -131,8 +145,9 @@ def selectInput(is_production):
 
 def main():
     test()
-    input_data = selectInput(False)
-    print('Day 7 Part 1:', getBottomProgram(input_data))
+    input_data = selectInput(True)
+    print('Day 7 Part 1:', getBottomProgram(input_data, 1))
+    print('Day 7 Part 2:', getBottomProgram(input_data, 2), new_balanced_weight)
 
 
 if __name__ == '__main__':
