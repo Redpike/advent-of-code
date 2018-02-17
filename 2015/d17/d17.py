6aefb297ccffad47b29140ed0a72f87410ffb784
Redpike <rsokulski@op.pl>
2018-02-17 15:52:36 +0100

    Advent of Code 2015 Day 17 Finished

--- 2015/d17/d17.py
+++ 2015/d17/d17.py
@@ -18,23 +18,28 @@ def read_input_file():
     return input_data
 
 
-def get_amount_of_combinations(input_data, liters):
+def get_amount_of_combinations(input_data, liters, part):
     combinations_dict = defaultdict(int)
     for i in range(len(input_data)):
         for j in combinations(input_data, i):
             if sum(j) == liters:
                 combinations_dict[i] += 1
-    return sum(combinations_dict.values())
+    if part == 1:
+        return sum(combinations_dict.values())
+    else:
+        return combinations_dict[min(combinations_dict.keys())]
 
 
 def test():
-    assert get_amount_of_combinations(test_input, 25) == 4
+    assert get_amount_of_combinations(test_input, 25, 1) == 4
+    assert get_amount_of_combinations(test_input, 25, 2) == 3
 
 
 def main():
     test()
     input_data = read_input_file()
-    print('Day 17 Part 1:', get_amount_of_combinations(input_data, 150))
+    print('Day 17 Part 1:', get_amount_of_combinations(input_data, 150, 1))
+    print('Day 17 Part 2:', get_amount_of_combinations(input_data, 150, 2))
 
 
 if __name__ == '__main__':
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 ### Advent of Code
 
-- [2015](2015) - written in Python (16.02.2018 16/25 done)
+- [2015](2015) - written in Python (17.02.2018 17/25 done)
 - [2017](2017) - written in Python, cause I wanted to learn it :) (finished 25.12.2017)
