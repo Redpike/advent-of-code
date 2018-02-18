73f714ec4cfcb34789bf4ab5e7a09ca934cf777b
Redpike <rsokulski@op.pl>
2018-02-18 10:00:25 +0100

    Advent of Code 2015 Day 18 Part 1 Finished

---
+++ 2015/d18/d18.py
@@ -0,0 +1,59 @@
+import numpy
+
+test_input = numpy.array([
+    [0, 1, 0, 1, 0, 1],
+    [0, 0, 0, 1, 1, 0],
+    [1, 0, 0, 0, 0, 1],
+    [0, 0, 1, 0, 0, 0],
+    [1, 0, 1, 0, 0, 1],
+    [1, 1, 1, 1, 0, 0]
+])
+
+
+def read_input_file():
+    return open('input', 'r').read().splitlines()
+
+
+def convert_data(input_data):
+    converted_data = [[int(element) for element in line.strip().replace(
+        '#', '1').replace('.', '0')] for line in input_data]
+    return numpy.array(converted_data)
+
+
+def update_grid(grid):
+    new = numpy.zeros(grid.shape)
+    for i in range(grid.shape[0]):
+        i_min = max(0, i - 1)
+        i_max = min(i + 1, grid.shape[0])
+        for j in range(grid.shape[1]):
+            oldval = grid[i, j]
+            j_min = max(0, j - 1)
+            j_max = min(j + 1, grid.shape[1])
+            on_neighbs = grid[i_min:i_max + 1, j_min:j_max + 1].sum() - oldval
+            if oldval == 1:
+                new[i, j] = int(on_neighbs in [2, 3])
+            else:
+                new[i, j] = int(on_neighbs == 3)
+    return new
+
+
+def get_on_lights(grid, steps):
+    for i in range(steps):
+        grid = update_grid(grid)
+    return int(grid.sum())
+
+
+def test():
+    assert get_on_lights(test_input, 4) == 4
+
+
+def main():
+    test()
+    input_data = read_input_file()
+    converted_data = convert_data(input_data)
+    print('Day 18 Part 1:', get_on_lights(converted_data, 100))
+    print('Day 18 Part 2:', )
+
+
+if __name__ == '__main__':
+    main()
