62b0528675772aeead8b7447524f174dd5319c6a
Redpike <rsokulski@op.pl>
2018-02-20 07:27:23 +0100

    Advent of Code 2015 Day 20 Part 1 Finished

---
+++ 2015/d20/d20.py
@@ -0,0 +1,29 @@
+from collections import defaultdict
+
+input_data = 29_000_000
+test_input = 150
+
+
+def get_the_lowest_house_number(_input, loops):
+    houses = defaultdict(int)
+
+    for elf in range(1, _input):
+        for house in range(elf, loops, elf):
+            houses[house] += (elf * 10)
+        if houses[elf] >= _input:
+            return elf
+
+
+def test():
+    assert get_the_lowest_house_number(140, 1_000) == 8
+
+
+def main():
+    test()
+    loops = 1_000_000
+    print('Day 20 Part 1:', get_the_lowest_house_number(input_data, loops))
+    print('Day 20 Part 2:',)
+
+
+if __name__ == '__main__':
+    main()
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 ### Advent of Code
 
-- [2015](2015) - written in Python (18.02.2018 18/25 done)
+- [2015](2015) - written in Python (20.02.2018 19,5/25 done - Day 19 Part 2 :()
 - [2017](2017) - written in Python, cause I wanted to learn it :) (finished 25.12.2017)
